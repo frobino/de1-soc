d947a3e5f5e3d2ce17c72792a315a42129db9a03
Francesco Robino <frobino@kth.se>
2014-10-09 17:25:08 +0200

    [INIT] Added files from Desktop at KTH
    Signed-off-by: Francesco Robino <frobino@kth.se>

---
+++ linux_software/blinker_with_kernel_module/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]){
+
+	if(argc==2){
+		FILE * pFile;
+		// File must be opened as "w", others such as "a+" 
+		// does not work (seg fault, both user and root)
+		pFile = fopen ("/sys/bus/platform/drivers/blinker/blinker" , "w");
+		if (pFile==NULL)
+   			printf("Failure opening file\n");
+
+		/* We remove the buffer from the file i/o (_IONBF). 
+		   No buffer is used. Each I/O operation is written 
+		   as soon as possible. In this case, the buffer and 
+		   size parameters are ignored.*/
+		unsigned char dummy;
+  		setvbuf(pFile,&dummy,_IONBF,1);
+
+		char value= argv[1][0]; // [1][0]: second string [1], char 0 [0]
+		printf("value: %c \n", value);
+
+		fprintf(pFile,"%c",value);
+		fclose(pFile);
+	}
+	else
+		printf("Wrong number of arguments\n");
+
+	return 0;
+}
