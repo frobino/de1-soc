67f57ae9e267347d1689538d61e54ef7c0e8c009
Francesco Robino <frobino@kth.se>
2014-10-27 09:09:56 +0100

    [CODE] Finalized ioctl and stevea
    RESULTS:
    	- stevea works properly but poorly documented and written;
    	- ioctl is well documented and well written, but the registration
    	  of the device in __init uses a old fashioned way that oblige
    	  the user to chmod 755 /dev/my_device or write a udev rule
    
    CONCLUSION (future points):
    	- a final_kernel version merging the two:
    		* start from ioctl and modify the __init as in stevea
    		* send structs
    
    
    
    Signed-off-by: Francesco Robino <frobino@kth.se>

--- linux_software/ioctl_kernel/my_device.c
+++ linux_software/ioctl_kernel/my_device.c
@@ -7,6 +7,7 @@
 #include <linux/types.h>
 #include <linux/device.h> // class_create
 #include <linux/ioctl.h> // to define _IOR and _IOW, see Documentation/ioctl/ioctl-number.txt
+// #include <linux/cdev.h> // to include if cdev_init is needed
 
 // #define MY_MACIG 'G'
 #define MY_MACIG ']'
@@ -17,6 +18,19 @@ static int major;
 static char msg[200];
 
 // ADDED
+// Trying to get access from user
+/*
+static int my_dev_uevent(struct device *dev, struct kobj_uevent_env *env)
+{
+    add_uevent_var(env, "DEVMODE=%#o", 0777);
+    return 0;
+}
+*/
+// Trying to get access from user
+/*
+static struct cdev c_dev; // Global variable for the character device structure
+*/
+
 static struct class *device_class;
 static dev_t devn;
 static struct device *ptr_error;
@@ -68,10 +82,14 @@ static struct file_operations fops = {
 	.unlocked_ioctl = device_ioctl,
 };
 
+static DRIVER_ATTR(my_device, S_IWOTH, device_read, device_write);
+MODULE_LICENSE("Dual BSD/GPL");
+
 static int __init cdevexample_module_init(void)
 {
 	/* register_chrdev */
-	major = register_chrdev(0, "my_device", &fops); // check if major is returned correctly
+	// Using 0 as first parameter, the kernel will dinamically assign a major number
+	major = register_chrdev(0, "my_device", &fops);
 	if (major < 0) {
      		printk ("Registering the character device failed with %d\n", major);
 	     	return major;
@@ -81,7 +99,8 @@ static int __init cdevexample_module_init(void)
 
 	// http://stackoverflow.com/questions/11765395/arm-char-device-driver-initialization-isnt-creating-dev-file
 
-	devn = MKDEV(major,0); //fix the minor: 
+	// We fix the minor number to 0, because my driver only drives this module
+	devn = MKDEV(major,0);
 	// http://stackoverflow.com/questions/12047946/trouble-calling-ioctl-from-user-space-c
 
 	/* class_create */
@@ -93,6 +112,12 @@ static int __init cdevexample_module_init(void)
     		return PTR_ERR(device_class);
     	}
 
+	// Trying to get access from user:
+	/*
+	// Instead of using udev rules, this line gives the module access from user space
+	device_class->dev_uevent = my_dev_uevent;
+	*/
+
 	/* device_create */
     	ptr_error = device_create(device_class, NULL, devn, NULL, "my_device");
     	if (IS_ERR(ptr_error))
@@ -102,6 +127,19 @@ static int __init cdevexample_module_init(void)
     		printk(KERN_INFO "Device creation failed\n");
     		return PTR_ERR(ptr_error);
     	}
+
+	/*added to test Udev*/
+	/*
+	int ret;
+	cdev_init(&c_dev, &fops);
+	if ((ret = cdev_add(&c_dev, major, 1)) < 0)
+	{
+		device_destroy(device_class, major);
+		class_destroy(device_class);
+		unregister_chrdev_region(major, 1);
+		return ret;
+	}
+	*/
 
 	return 0;
 }
@@ -117,4 +155,3 @@ static void __exit cdevexample_module_exit(void)
 
 module_init(cdevexample_module_init);
 module_exit(cdevexample_module_exit);
-MODULE_LICENSE("GPL");
